708b614d89eee6e70afc2d874fa9f709584157ce
William Foschiera <wfoschiera@gmail.com>
2021-08-18 17:25:45 -0400

    create models Airport and Flights

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.city} ({self.code})"
+
+
 class Flight(models.Model):
-    
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.origin} to {self.destination}"
